42962714f1e395eec2b78b37eae0ef1a0514f531
master <master@SMLAB108PC06_23>
2023-07-06 20:32:42 -0500

    Agregar archivos de proyecto.

---
+++ jherreraS6/Insertar.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace jherreraS6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Insertar : ContentPage
+    {
+        public Insertar()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngresar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://192.168.10.39:8080/uisrael/post.php", "POST", parametros);
+                var mensaje = "Dato Ingresado con exito";
+                DependencyService.Get<Mensaje>().longAlert(mensaje);
+
+                await Navigation.PushAsync(new MainPage());
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("alerta", "Error" + ex.Message, "ok");
+            }
+        }
+
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
---
+++ jherreraS6/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace jherreraS6
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string URL = "http://192.168.10.39:8080/uisrael/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<estudiante> _post;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+            Obtener();
+        }
+        public async void Obtener()
+        {
+            var content = await client.GetStringAsync(URL);
+            List<estudiante> post = JsonConvert.DeserializeObject<List<estudiante>>(content);
+            ListaEstudiantes.ItemsSource = post;
+            _post = new ObservableCollection<estudiante>(post);
+
+        }
+
+        private async void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Insertar());
+        }
+
+
+        private void ListaEstudiantes_ItemSelected_1(object sender, SelectedItemChangedEventArgs e)
+        {
+            var objetoEstudiante = (estudiante)e.SelectedItem;
+            var objeto = (estudiante)e.SelectedItem;
+            var item = objeto.codigo.ToString();
+            int codigo = Convert.ToInt32(item);
+            var nombre = objeto.nombre.ToString();
+            var apellido = objeto.apellido.ToString();
+            var itemedad = objeto.edad.ToString();
+            int edad = Convert.ToInt32(itemedad);
+            Navigation.PushAsync(new ActEliminar(objetoEstudiante));
+        }
+    }
+}
